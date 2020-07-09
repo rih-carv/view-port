3075578303fa7807cbf5f0558b529dff85bf5461
Ricardo Carvalho <ricardo.carvalho@dextra-sw.com>
2020-07-08 22:32:45 -0300

    Adjustments to the ViewPortSavedState Parcelable implementation

--- commons/src/main/java/com/github/globocom/viewport/commons/ViewPortSavedState.kt
+++ commons/src/main/java/com/github/globocom/viewport/commons/ViewPortSavedState.kt
@@ -1,13 +1,24 @@
 package com.github.globocom.viewport.commons
 
+import android.os.Build
 import android.os.Parcel
 import android.os.Parcelable
 import android.view.View
+import androidx.annotation.RequiresApi
 
 class ViewPortSavedState : View.BaseSavedState {
     companion object {
-        val CREATOR: Parcelable.Creator<ViewPortSavedState?> =
-            object : Parcelable.Creator<ViewPortSavedState?> {
+        @JvmField
+        val CREATOR: Parcelable.ClassLoaderCreator<ViewPortSavedState?> =
+            object : Parcelable.ClassLoaderCreator<ViewPortSavedState?> {
+                @RequiresApi(Build.VERSION_CODES.N)
+                override fun createFromParcel(
+                    source: Parcel,
+                    loader: ClassLoader?
+                ): ViewPortSavedState? {
+                    return ViewPortSavedState(source, loader)
+                }
+
                 override fun createFromParcel(`in`: Parcel): ViewPortSavedState? {
                     return ViewPortSavedState(`in`)
                 }
@@ -26,21 +37,29 @@ class ViewPortSavedState : View.BaseSavedState {
 
     constructor(superState: Parcelable?) : super(superState)
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    private constructor(parcel: Parcel, loader: ClassLoader?) : super(parcel, loader) {
+        setupWith(parcel)
+    }
+
     private constructor(parcel: Parcel) : super(parcel) {
+        setupWith(parcel)
+    }
+
+    private fun setupWith(parcel: Parcel) {
         isHearBeatStarted = parcel.readByte() != 0.toByte()
         isLibStarted = parcel.readByte() != 0.toByte()
-        currentVisibleItemsList =
-            (parcel.createIntArray() ?: mutableListOf<Int>()) as MutableList<Int>
-        previouslyVisibleItemsList = (parcel.createIntArray() ?: mutableListOf<Int>()) as MutableList<Int>
-        oldItemsList = (parcel.createIntArray() ?: mutableListOf<Int>()) as MutableList<Int>
+        currentVisibleItemsList = (parcel.createIntArray()?.toMutableList() ?: mutableListOf())
+        previouslyVisibleItemsList = (parcel.createIntArray()?.toMutableList() ?: mutableListOf())
+        oldItemsList = (parcel.createIntArray()?.toMutableList() ?: mutableListOf())
     }
 
     override fun writeToParcel(out: Parcel, flags: Int) {
         super.writeToParcel(out, flags)
         out.writeByte(if (isHearBeatStarted) 1 else 0)
         out.writeByte(if (isLibStarted) 1 else 0)
-        out.writeList(currentVisibleItemsList as MutableList<*>?)
-        out.writeList(previouslyVisibleItemsList as MutableList<*>?)
-        out.writeList(oldItemsList as MutableList<*>?)
+        out.writeIntArray(currentVisibleItemsList.toIntArray())
+        out.writeIntArray(previouslyVisibleItemsList.toIntArray())
+        out.writeIntArray(oldItemsList.toIntArray())
     }
 }
